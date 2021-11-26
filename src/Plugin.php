a84e8f8c40447675afc2f46477e2e00384e94a25
Rémy Perona <remperona@gmail.com>
2021-11-26 10:51:37 -0500

    add the container as an injected dependency with filter

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,11 +8,38 @@ use RocketCDN\EventManagement\EventManager;
 
 class Plugin {
 	/**
+	 * Instance of Container class.
+	 *
+	 * @var Container instance
+	 */
+	private $container;
+
+	/**
 	 * Is the plugin loaded
 	 *
 	 * @var boolean
 	 */
 	private $loaded = false;
+
+	/**
+	 * Creates an instance of the Plugin.
+	 *
+	 * @param Container $container     Instance of the container.
+	 */
+	public function __construct( Container $container ) {
+		$this->container = $container;
+
+		add_filter( 'rocketcdn_container', [ $this, 'get_container' ] );
+	}
+
+	/**
+	 * Returns the RocketCDN container instance.
+	 *
+	 * @return Container
+	 */
+	public function get_container() {
+		return $this->container;
+	}
 
 	/**
 	 * Checks if the plugin is loaded
@@ -33,9 +60,7 @@ class Plugin {
 			return;
 		}
 
-		$container = new Container();
-
-		$container->add(
+		$this->container->add(
 			'event_manager',
 			function () {
 				return new EventManager();
