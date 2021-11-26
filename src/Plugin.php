8232d013aff58abd57492e0855c7264f5640f09d
Rémy Perona <remperona@gmail.com>
2021-11-26 10:46:03 -0500

    manage loading of service providers and subscribers

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 namespace RocketCDN;
 
 use RocketCDN\Dependencies\League\Container\Container;
+use RocketCDN\Dependencies\League\Container\ServiceProvider\ServiceProviderInterface;
 use RocketCDN\EventManagement\EventManager;
+use RocketCDN\EventManagement\SubscriberInterface;
 
 class Plugin {
+	/**
+	 * Container instance.
+	 *
+	 * @var Container
+	 */
+	private $container;
+
 	/**
 	 * Is the plugin loaded
 	 *
@@ -33,15 +42,55 @@ class Plugin {
 			return;
 		}
 
-		$container = new Container();
+		$this->container = new Container();
 
-		$container->add(
+		$this->container->add(
 			'event_manager',
 			function () {
 				return new EventManager();
 			}
 			);
 
+		foreach ( $this->get_service_providers() as $service_provider ) {
+			$service_provider_instance = new $service_provider();
+			$this->container->addServiceProvider( $service_provider_instance );
+
+			// Load each service provider's subscribers if found.
+			$this->load_subscribers( $service_provider_instance );
+		}
+
 		$this->loaded = true;
+	}
+
+	/**
+	 * Get list of service providers' classes.
+	 *
+	 * @return array Service providers.
+	 */
+	private function get_service_providers() {
+		return [
+			'RocketCDN\Admin\Settings\ServiceProvider',
+		];
+	}
+
+	/**
+	 * Load list of event subscribers from service provider.
+	 *
+	 * @param ServiceProviderInterface $service_provider_instance Instance of service provider.
+	 *
+	 * @return void
+	 */
+	private function load_subscribers( ServiceProviderInterface $service_provider_instance ) {
+		if ( empty( $service_provider_instance->subscribers ) ) {
+			return;
+		}
+
+		foreach ( $service_provider_instance->subscribers as $subscriber ) {
+			$subscriber_object = $this->container->get( $subscriber );
+
+			if ( $subscriber_object instanceof SubscriberInterface ) {
+				$this->container->get( 'event_manager' )->add_subscriber( $subscriber_object );
+			}
+		}
 	}
 }
