724d16b05b2cbc30978a089694ae67a78254c764
Rémy Perona <remperona@gmail.com>
2021-11-29 16:30:14 -0500

    add the settings page classes

---
+++ src/Admin/Settings/ServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace RocketCDN\Admin\Settings;
+
+use RocketCDN\Dependencies\League\Container\ServiceProvider\AbstractServiceProvider;
+
+class ServiceProvider extends AbstractServiceProvider {
+	/**
+	 * Services provided by this provider
+	 *
+	 * @var array
+	 */
+	protected $provides = [
+		'admin_page',
+		'admin_subscriber',
+	];
+
+	/**
+	 * Subscribers provided by this provider
+	 *
+	 * @var array
+	 */
+	public $subscribers = [
+		'admin_subscriber',
+	];
+
+	/**
+	 * Registers the provided classes
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->getContainer()->add( 'admin_page', 'RocketCDN\Admin\Settings\Page' )
+			->addArgument( $this->getContainer()->get( 'options' ) )
+			->addArgument( $this->getContainer()->get( 'template_basepath' ) );
+		$this->getContainer()->add( 'admin_subscriber', 'RocketCDN\Admin\Settings\Subscriber' )
+			->addArgument( $this->getContainer()->get( 'admin_page' ) );
+	}
+}
