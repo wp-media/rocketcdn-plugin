3888131b4af664c7433cf2cfaadc24672ae59245
Rémy Perona <remy@wp-rocket.me>
2021-12-01 15:57:49 -0500

    Add the admin notices component (PR #5)

---
+++ src/Admin/Notices/Notices.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types=1);
+
+namespace RocketCDN\Admin\Notices;
+
+use RocketCDN\Options\Options;
+
+class Notices {
+	/**
+	 * Options instance
+	 *
+	 * @var Options
+	 */
+	private $options;
+
+	/**
+	 * Instantiates the class
+	 *
+	 * @param Options $options Options instance.
+	 */
+	public function __construct( Options $options ) {
+		$this->options = $options;
+	}
+
+	/**
+	 * Displays a notice if the API key is empty
+	 *
+	 * @return void
+	 */
+	public function empty_api_key_notice() {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		if ( $this->options->get( 'api_key' ) ) {
+			return;
+		}
+
+		$message = sprintf(
+			// translators: %1$s = strong opening tag, %2$s = strong closing tag, %3$s = link opening tag, %4$s = link closing tag.
+			__( '%1$sRocketCDN:%2$s You are almost done. %3$sAdd your API key%4$s to deliver your content at the speed of light!', 'rocketcdn' ),
+			'<strong>',
+			'</strong>',
+			'<a href="' . admin_url( 'options-general.php?page=rocketcdn' ) . '">',
+			'</a>'
+		);
+
+		echo '<div class="notice notice-warning"><p>' . $message . '</p></div>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+
+	/**
+	 * Displays a notice if the API key is wrong
+	 *
+	 * @return void
+	 */
+	public function wrong_api_key_notice() {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		if ( ! $this->options->get( 'api_key' ) ) {
+			return;
+		}
+
+		$message = sprintf(
+			// translators: %1$s = strong opening tag, %2$s = strong closing tag, %3$s = link opening tag, %4$s = link closing tag.
+			__( '%1$sRocketCDN:%2$s Your API key is wrong. You can find your API key in your %3$sRocketCDN account%4$s', 'rocketcdn' ),
+			'<strong>',
+			'</strong>',
+			'<a href="https://rocketcdn.me/account/sites/">',
+			'</a>'
+		);
+
+		echo '<div class="notice notice-error"><p>' . $message . '</p></div>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+}
---
+++ src/Admin/Notices/ServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace RocketCDN\Admin\Notices;
+
+use RocketCDN\Dependencies\League\Container\ServiceProvider\AbstractServiceProvider;
+
+class ServiceProvider extends AbstractServiceProvider {
+	/**
+	 * Services provided by this provider
+	 *
+	 * @var array
+	 */
+	protected $provides = [
+		'admin_notices',
+		'admin_notices_subscriber',
+	];
+
+	/**
+	 * Subscribers provided by this provider
+	 *
+	 * @var array
+	 */
+	public $subscribers = [
+		'admin_notices_subscriber',
+	];
+
+	/**
+	 * Registers the provided classes
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->getContainer()->add( 'admin_notices', 'RocketCDN\Admin\Notices\Notices' )
+			->addArgument( $this->getContainer()->get( 'options' ) );
+		$this->getContainer()->add( 'admin_notices_subscriber', 'RocketCDN\Admin\Notices\Subscriber' )
+			->addArgument( $this->getContainer()->get( 'admin_notices' ) );
+	}
+}
