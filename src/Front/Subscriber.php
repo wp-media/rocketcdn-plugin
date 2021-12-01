295505db3158a7fbde7150db4b0c3d7c1bc88fc9
Rémy Perona <remperona@gmail.com>
2021-12-01 16:39:31 -0500

    add preconnect to resources hints

--- src/Front/Subscriber.php
+++ src/Front/Subscriber.php
@@ -30,6 +30,7 @@ class Subscriber implements SubscriberInterface {
 	public static function get_subscribed_events() {
 		return [
 			'template_redirect' => [ 'start_buffering', 2 ],
+			'wp_resource_hints' => [ 'add_preconnect_cdn', 10, 2 ],
 		];
 	}
 
@@ -42,5 +43,19 @@ class Subscriber implements SubscriberInterface {
 	 */
 	public function start_buffering() {
 		$this->cdn->start_buffering();
+	}
+
+	/**
+	 * Adds a preconnect tag for the CDN.
+	 *
+	 * @since 1.0
+	 *
+	 * @param array  $urls          The initial array of wp_resource_hint urls.
+	 * @param string $relation_type The relation type for the hint: eg., 'preconnect', 'prerender', etc.
+	 *
+	 * @return array
+	 */
+	public function add_preconnect_cdn( array $urls, string $relation_type ): array {
+		return $this->cdn->add_preconnect_cdn( $urls, $relation_type );
 	}
 }
