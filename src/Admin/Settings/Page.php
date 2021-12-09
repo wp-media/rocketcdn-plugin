486611ebaae30e56f9cc4a03fb95087b1e742bef
Rémy Perona <remperona@gmail.com>
2021-12-09 15:33:57 -0500

    add new methods to handle API key and CDN URL validation

--- src/Admin/Settings/Page.php
+++ src/Admin/Settings/Page.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace RocketCDN\Admin\Settings;
 
 use RocketCDN\Options\Options;
+use RocketCDN\API\Client;
 
 class Page {
 	/**
@@ -14,6 +15,13 @@ class Page {
 	private $options;
 
 	/**
+	 * API Client instance
+	 *
+	 * @var Client
+	 */
+	private $api_client;
+
+	/**
 	 * Templates base path
 	 *
 	 * @var string
@@ -21,14 +29,25 @@ class Page {
 	private $template_basepath;
 
 	/**
+	 * Assets base URL
+	 *
+	 * @var string
+	 */
+	private $assets_baseurl;
+
+	/**
 	 * Instantiates the class
 	 *
 	 * @param Options $options Options instance.
+	 * @param Client  $api_client API Client instance.
 	 * @param string  $template_basepath Base filepath for the template.
+	 * @param string  $assets_baseurl Base URL for the assets.
 	 */
-	public function __construct( Options $options, $template_basepath ) {
+	public function __construct( Options $options, Client $api_client, $template_basepath, $assets_baseurl ) {
 		$this->options           = $options;
+		$this->api_client        = $api_client;
 		$this->template_basepath = $template_basepath;
+		$this->assets_baseurl    = $assets_baseurl;
 	}
 
 	/**
@@ -73,5 +92,127 @@ class Page {
 		}
 
 		include $template_path;
+	}
+
+	public function validate_api_key() {
+		check_ajax_referer( 'rocketcdn_ajax', 'nonce' );
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_send_json_error( __( 'You do not have permissions to perform this action.', 'rocketcdn' ) );
+		}
+
+		if ( ! isset( $_POST['api_key'] ) ) {
+			wp_send_json_error( __( 'They API key field is not set', 'rocketcdn' ) );
+		}
+
+		$api_key = sanitize_key( $_POST['api_key'] );
+
+		$valid_key = $this->api_client->get_customer_data( $api_key );
+
+		if ( empty( $valid_key ) ) {
+			wp_send_json_error( __( 'Invalid API key', 'rocketcdn' ) );
+		}
+
+		$is_sync = $this->api_client->is_website_sync( $api_key );
+
+		if ( ! $is_sync ) {
+			$message = sprintf(
+				// translators: %1$s = opening link tag, %2$s = closing link tag.
+				__( 'Your website is not yet synchronized with your subscription. Please add your website from your %1$sRocketCDN account page%2$s.', 'rocketcdn' ),
+				'<a href="https://rocketcdn.me/account/sites/" target="_blank" rel="noopener">',
+				'</a>'
+			);
+
+			wp_send_json_error( $message );
+		}
+
+		wp_send_json_success();
+	}
+
+	public function update_api_key() {
+		check_ajax_referer( 'rocketcdn_ajax', 'nonce' );
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_send_json_error( __( 'You do not have permissions to perform this action.', 'rocketcdn' ) );
+		}
+
+		if ( ! isset( $_POST['api_key'] ) ) {
+			wp_send_json_error( __( 'They API key field is not set', 'rocketcdn' ) );
+		}
+
+		$api_key = sanitize_key( $_POST['api_key'] );
+
+		if ( empty( $api_key ) ) {
+			wp_send_json_error( __( 'The API key field is empty', 'rocketcdn' ) );
+		}
+
+		$valid_key = $this->api_client->get_customer_data( $api_key );
+
+		if ( empty( $valid_key ) ) {
+			wp_send_json_error( __( 'Invalid API key', 'rocketcdn' ) );
+		}
+
+		$is_sync = $this->api_client->is_website_sync( $api_key );
+
+		if ( ! $is_sync ) {
+			$message = sprintf(
+				// translators: %1$s = opening link tag, %2$s = closing link tag.
+				__( 'Your website is not yet synchronized with your subscription. Please add your website from your %1$sRocketCDN account page%2$s.', 'rocketcdn' ),
+				'<a href="https://rocketcdn.me/account/sites/" target="_blank" rel="noopener">',
+				'</a>'
+			);
+
+			wp_send_json_error( $message );
+		}
+
+		$this->options->set( 'api_key', $api_key );
+
+		wp_send_json_success( __( 'Your API key is valid.', 'rocketcdn' ) );
+	}
+
+	public function save_cdn_url() {
+		$cdn_url = $this->api_client->get_website_cdn_url();
+
+		if ( empty( $cdn_url ) ) {
+			return;
+		}
+
+		$this->options->set( 'cdn_url', $cdn_url );
+	}
+
+	public function purge_cache() {
+		check_ajax_referer( 'rocketcdn_ajax', 'nonce' );
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_send_json_error( __( 'You do not have permissions to perform this action.', 'rocketcdn' ) );
+		}
+
+		$result = $this->api_client->purge_cache();
+
+		if ( ! $result['success'] ) {
+			wp_send_json_error( $result['message'] );
+		}
+
+		wp_send_json_success( __( 'Done! Your cache has been cleared', 'rocketcdn' ) );
+	}
+
+	/**
+	 * Enqueue the assets for the settings page
+	 *
+	 * @param string $hook_suffix Current page hook.
+	 */
+	public function enqueue_assets( $hook_suffix ) {
+		if ( 'settings_page_rocketcdn' !== $hook_suffix ) {
+			return;
+		}
+
+		wp_enqueue_script( 'rocketcdn_ajax', $this->assets_baseurl . 'js/ajax.js', [], '1.0', true );
+		wp_localize_script(
+			'rocketcdn_ajax',
+			'rocketcdn_ajax_data',
+			[
+				'nonce' => wp_create_nonce( 'rocketcdn_ajax' ),
+			]
+		);
 	}
 }
