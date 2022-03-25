841b503573ddae405cc085f4df873d72eb40f50b
COQUARD Cyrille <cyrille@wp-media.me>
2022-03-25 17:08:44 +0100

    Added Unit tests for Notices class

---
+++ tests/Fixtures/src/Notices/Notices/emptyApiKeyNotice.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'testUserHasNoRightShouldNoPrintAPINotice' => [
+        'config' => [
+            'api' => 'api',
+            'has_rights' => false,
+            'admin_url' => 'admin_url',
+        ],
+        'expected' => '',
+    ],
+    'testAPIKeyExistShouldNoPrintAPINotice' => [
+        'config' => [
+            'api' => 'api',
+            'has_rights' => true,
+            'admin_url' => 'admin_url',
+        ],
+        'expected' => '',
+    ],
+    'testNoAPIAndHasRightShouldPrintAPINotice' => [
+        'config' => [
+            'api' => '',
+            'has_rights' => true,
+            'admin_url' => 'admin_url',
+        ],
+        'expected' => file_get_contents( WP_ROCKET_CDN_TESTS_FIXTURES_DIR . '/src/Notices/Notices/HTML/testNoAPIAndHasRightShouldPrintAPINotice.html' ),
+    ]
+];
---
+++ tests/Fixtures/src/Notices/Notices/wrongApiKeyNotice.php
@@ -0,0 +1,42 @@
+<?php
+return [
+    'testUserHasNoRightShouldNoPrintWarning' => [
+        'config' => [
+            'api' => '',
+            'has_rights' => false,
+            'data' => [
+                'data'
+            ]
+        ],
+        'expected' => '',
+    ],
+    'testAPIKeyNotExistShouldNoPrintWarning' => [
+        'config' => [
+            'api' => '',
+            'has_rights' => true,
+            'data' => [
+                'data'
+            ]
+        ],
+        'expected' => '',
+    ],
+    'testNoUserdataShouldNoPrintWarning' => [
+        'config' => [
+            'api' => 'api',
+            'has_rights' => true,
+            'data' => [
+                'data'
+            ]
+        ],
+        'expected' => '',
+    ],
+    'testAPIKeyUserdataHasRightsShouldPrintWarning' => [
+        'config' => [
+            'api' => 'api',
+            'has_rights' => true,
+            'data' => [
+            ]
+        ],
+        'expected' => file_get_contents( WP_ROCKET_CDN_TESTS_FIXTURES_DIR . '/src/Notices/Notices/HTML/testAPIKeyUserdataHasRightsShouldPrintWarning.html' ),
+    ],
+];
