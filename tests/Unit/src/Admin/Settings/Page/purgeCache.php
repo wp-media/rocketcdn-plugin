2b0d2d068e67303386dc3bb09512f67cf8431f02
COQUARD Cyrille <cyrille@wp-media.me>
2022-03-31 17:43:20 +0200

    Added Unit tests for Page class

---
+++ tests/Unit/src/Admin/Settings/Page/purgeCache.php
@@ -0,0 +1,39 @@
+<?php
+
+use RocketCDN\Admin\Settings\Page;
+use RocketCDN\API\Client;
+use RocketCDN\Options\Options;
+use RocketCDN\Tests\Unit\TestCase;
+use Brain\Monkey\Functions;
+
+class Test_PurgeCache extends TestCase {
+    protected $options;
+    protected $client;
+    protected $page;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->options = Mockery::mock( Options::class);
+        $this->client = Mockery::mock( Client::class);
+        $this->page = new Page($this->options, $this->client,  WP_ROCKET_CDN_PLUGIN_ROOT . '/views/', '/');
+    }
+
+    /**
+     * @dataProvider configTestData
+     */
+    public function testShouldReturnAsExcepted($config, $expected) {
+        Functions\expect( '__' )->zeroOrMoreTimes()->andReturnFirstArg();
+        Functions\expect( 'current_user_can' )->with('manage_options')->andReturn($config['has_right']);
+        Functions\expect( 'check_ajax_referer' );
+        if($config['has_right']) {
+            $this->client->expects()->purge_cache()->andReturn($config['response']);
+        }
+        if($expected['success']) {
+            Functions\expect( 'wp_send_json_success' )->with($expected['message']);
+        } else {
+            Functions\expect( 'wp_send_json_error' )->with($expected['message']);
+        }
+        $this->page->purge_cache();
+    }
+}
---
+++ tests/Unit/src/Admin/Settings/Page/saveCdnUrl.php
@@ -0,0 +1,31 @@
+<?php
+
+use RocketCDN\Admin\Settings\Page;
+use RocketCDN\API\Client;
+use RocketCDN\Options\Options;
+use RocketCDN\Tests\Unit\TestCase;
+
+class Test_SaveCdnUrl extends TestCase {
+    protected $options;
+    protected $client;
+    protected $page;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->options = Mockery::mock( Options::class);
+        $this->client = Mockery::mock( Client::class);
+        $this->page = new Page($this->options, $this->client,  WP_ROCKET_CDN_PLUGIN_ROOT . '/views/', '/');
+    }
+
+    /**
+     * @dataProvider configTestData
+     */
+    public function testShouldReturnAsExcepted($config) {
+        $this->client->expects()->get_website_cdn_url()->andReturn($config['cdn']);
+        if($config['cdn']) {
+            $this->options->expects()->set('cdn_url', $config['cdn']);
+        }
+        $this->page->save_cdn_url();
+    }
+}
