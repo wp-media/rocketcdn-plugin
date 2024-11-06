5c6e9e112fb0bf443b68ab645a080164d4a9cbe9
Rémy Perona <remperona@gmail.com>
2024-11-06 11:52:18 -0500

    fix code style

--- src/Activation/Activation.php
+++ src/Activation/Activation.php
@@ -8,15 +8,15 @@ use RocketCDN\Dependencies\LaunchpadFrameworkOptions\Interfaces\OptionsAwareInte
 use RocketCDN\Dependencies\LaunchpadFrameworkOptions\Traits\OptionsAwareTrait;
 
 class Activation implements ActivationInterface, OptionsAwareInterface {
-    use OptionsAwareTrait;
-    /**
-     * Executes this method on plugin activation
-     *
-     * @return void
-     */
-    public function activate() {
-        if ( '' === $this->options->get( 'current_version', '' ) ) {
-            $this->options->set( 'current_version', rocketcdn_get_constant( 'ROCKETCDN_VERSION', '' ) );
-        }
-    }
+	use OptionsAwareTrait;
+	/**
+	 * Executes this method on plugin activation
+	 *
+	 * @return void
+	 */
+	public function activate() {
+		if ( '' === $this->options->get( 'current_version', '' ) ) {
+			$this->options->set( 'current_version', rocketcdn_get_constant( 'ROCKETCDN_VERSION', '' ) );
+		}
+	}
 }
--- src/Activation/ServiceProvider.php
+++ src/Activation/ServiceProvider.php
@@ -7,23 +7,23 @@ use RocketCDN\Dependencies\LaunchpadCore\Container\AbstractServiceProvider;
 use RocketCDN\Dependencies\LaunchpadCore\Activation\HasActivatorServiceProviderInterface;
 
 class ServiceProvider extends AbstractServiceProvider implements HasActivatorServiceProviderInterface {
-    /**
+	/**
 	 * Returns list of activators.
 	 *
 	 * @return string[]
 	 */
 	public function get_activators(): array {
-        return [
-            Activation::class,
-        ];
-    }
+		return [
+			Activation::class,
+		];
+	}
 
-    /**
+	/**
 	 * Register the services.
 	 *
 	 * @return void
 	 */
-    public function define(): void {
-        $this->register_service( Activation::class );
-    }
+	public function define() {
+		$this->register_service( Activation::class );
+	}
 }
