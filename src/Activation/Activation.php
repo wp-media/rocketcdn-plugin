e032ffa654d4ecdd30e845bb913f7538e7f29e01
Rémy Perona <remperona@gmail.com>
2024-11-06 11:49:30 -0500

    save version on activation if not yet set

---
+++ src/Activation/Activation.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace RocketCDN\Activation;
+
+use RocketCDN\Dependencies\LaunchpadCore\Activation\ActivationInterface;
+use RocketCDN\Dependencies\LaunchpadFrameworkOptions\Interfaces\OptionsAwareInterface;
+use RocketCDN\Dependencies\LaunchpadFrameworkOptions\Traits\OptionsAwareTrait;
+
+class Activation implements ActivationInterface, OptionsAwareInterface {
+    use OptionsAwareTrait;
+    /**
+     * Executes this method on plugin activation
+     *
+     * @return void
+     */
+    public function activate() {
+        if ( '' === $this->options->get( 'current_version', '' ) ) {
+            $this->options->set( 'current_version', rocketcdn_get_constant( 'ROCKETCDN_VERSION', '' ) );
+        }
+    }
+}
